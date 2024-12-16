c693988636d7ff459a871839d077d713ba60b91e
PTCanh <142814031+PTCanh@users.noreply.github.com>
2024-12-16 20:40:51 +0700

    deploy database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function connectMongoDB() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/HoteLuxury");
+        //await mongoose.connect("mongodb://localhost:27017/HoteLuxury");
+        await mongoose.connect(process.env.MONGO_ATLAS_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
