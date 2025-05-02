2ee89fc674364be02e532dfe41bd25c348859b5c
PTCanh <142814031+PTCanh@users.noreply.github.com>
2025-05-02 13:32:02 +0700

    fix payment return

--- src/config/momoConfig.js
+++ src/config/momoConfig.js
@@ -3,8 +3,10 @@ const momoConfig = {
     accessKey: 'F8BBA842ECF85',
     secretKey: 'K951B6PE1waDMi640xX08PD3vg6EkVlz',
     requestType: 'payWithMethod',
-    redirectUrl: 'https://be-hote-luxury.vercel.app/booking/momo_return',
-    ipnUrl: 'https://be-hote-luxury.vercel.app/booking/momo_ipn',
+    //redirectUrl: 'https://be-hote-luxury.vercel.app/booking/momo_return',
+    redirectUrl: 'http://localhost:9000/booking/momo_return',
+    //ipnUrl: 'https://be-hote-luxury.vercel.app/booking/momo_ipn',
+    ipnUrl: 'http://localhost:9000/booking/momo_ipn',
     endpoint: 'https://test-payment.momo.vn/v2/gateway/api/create'
 };
 
